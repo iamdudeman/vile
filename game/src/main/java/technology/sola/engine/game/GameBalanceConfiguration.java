31c6119df0d354ce0c3cfba437cc9fe0e50635fa
iamdudeman <timothy.solum@gmail.com>
2024-07-19 16:52:26 -0500

    More logic to VialsBoard

--- game/src/main/java/technology/sola/engine/game/GameBalanceConfiguration.java
+++ game/src/main/java/technology/sola/engine/game/GameBalanceConfiguration.java
@@ -1,6 +1,7 @@
 package technology.sola.engine.game;
 
 public class GameBalanceConfiguration {
+  public static final int INITIAL_LIVES = 2;
   public static final int VIAL_COUNT = 3;
   public static final int VIAL_DEPTH = 4;
 
--- game/src/main/java/technology/sola/engine/game/state/VialsBoard.java
+++ game/src/main/java/technology/sola/engine/game/state/VialsBoard.java
@@ -2,7 +2,12 @@ package technology.sola.engine.game.state;
 
 import technology.sola.engine.game.GameBalanceConfiguration;
 
+import java.util.Random;
+
 public class VialsBoard {
+  private static final int NEUTRAL_PH = 7;
+  private static final int MAX_PH = 14;
+  private final Random random = new Random();
   private final Vial[] playerVials;
   private final Vial[] opponentVials;
 
@@ -18,11 +23,58 @@ public class VialsBoard {
     }
   }
 
+  public int rollNextPh() {
+    return random.nextInt(MAX_PH + 1);
+  }
+
   public Vial[] getPlayerVials() {
     return playerVials;
   }
 
   public Vial[] getOpponentVials() {
     return opponentVials;
+  }
+
+  public float getPlayerScore() {
+    return calculateScore(this.playerVials);
+  }
+
+  public float getOpponentScore() {
+    return calculateScore(this.opponentVials);
+  }
+
+  public boolean isPlayerWin() {
+    float playerNeutralDiff = Math.abs(NEUTRAL_PH - getPlayerScore());
+    float opponentNeutralDiff = Math.abs(NEUTRAL_PH - getOpponentScore());
+
+    return playerNeutralDiff <= opponentNeutralDiff;
+  }
+
+  public boolean isBoardFull() {
+    for (var vial : getPlayerVials()) {
+      if (!vial.isFull()) {
+        return false;
+      }
+    }
+
+    for (var vial : getOpponentVials()) {
+      if (!vial.isFull()) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  private float calculateScore(Vial[] vials) {
+    float score = 0;
+
+    for (var vial : vials) {
+      score += vial.getScore();
+    }
+
+    score /= vials.length;
+
+    return score;
   }
 }
